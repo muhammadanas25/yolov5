d324fd6181c8c35510fc736ef1d948ec45991308
manassiddiqui <manassiddiquifolio3.com>
2022-12-02 15:58:33 +0500

    path changes

--- train.py
+++ train.py
@@ -40,8 +40,8 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-print(os.environ['SM_CHANNEL_TRAIN'])
-if Path(os.environ['SM_CHANNEL_TRAIN']+ '/data.yaml').exists():
+print(f"--------------------------------{os.environ['SM_CHANNEL_DATA']}---------------------")
+if Path(os.environ['SM_CHANNEL_DATA']).exists():
     print("exist")
 
 import val as validate  # for end-of-epoch mAP
@@ -439,7 +439,7 @@ def parse_opt(known=False):
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default=ROOT / 'yolov5s.pt', help='initial weights path')
     parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
-    parser.add_argument('--data', type=str, default=  os.environ['SM_CHANNEL_TRAIN'] + '/data.yaml', help='dataset.yaml path')
+    parser.add_argument('--data', type=str, default=  os.environ['SM_CHANNEL_DATA'] , help='dataset.yaml path')
     parser.add_argument('--hyp', type=str, default=ROOT / 'data/hyps/hyp.scratch-low.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=100, help='total training epochs')
     parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs, -1 for autobatch')
